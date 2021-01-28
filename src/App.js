90eca775acb7ff5893fcea18c49c133b04cb5aa2
Ozan Ozenoglu <ozan.ozenoglu@argela.com.tr>
2021-01-28 16:49:39 +0300

    I changed the text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There Man!
+          I changed this line
         </p>
         <a
           className="App-link"
